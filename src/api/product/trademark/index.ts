0f978c57f8e5ea7673e72f6269ede1f8516a17bf
Rika-L <2209393334@qq.com>
2024-02-22 23:20:11 +0800

    feat: 完成品牌管理的增、查业务

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,29 +1,35 @@
 //品牌管理模块的接口
 import request from "@/utils/request.ts";
-import {TradeMark, TradeMarkResponseData} from "@/api/product/trademark/type.ts";
+import {
+  TradeMark,
+  TradeMarkResponseData,
+} from "@/api/product/trademark/type.ts";
 
 //接口地址
 enum API {
-    //获取已有品牌接口
-    TRADEMARK_URL = "/admin/product/baseTrademark/",
-    //添加品牌
-    ADD_TRADEMARK_URL = "/admin/product/baseTrademark/save",
-    //修改品牌
-    UPDATE_TRADEMARK_URL = "/admin/product/baseTrademark/update",
+  //获取已有品牌接口
+  TRADEMARK_URL = "/admin/product/baseTrademark/",
+  //添加品牌
+  ADD_TRADEMARK_URL = "/admin/product/baseTrademark/save",
+  //修改品牌
+  UPDATE_TRADEMARK_URL = "/admin/product/baseTrademark/update",
 }
 
 //获取已有品牌的接口方法
 //page获取第几页的数据
 //limit获取几个数据
-export const reqHasTrademark = (page: number, limit: number) => request.get<any, TradeMarkResponseData>(API.TRADEMARK_URL + `${page}/${limit}`);
+export const reqHasTrademark = (page: number, limit: number) =>
+  request.get<any, TradeMarkResponseData>(
+    API.TRADEMARK_URL + `${page}/${limit}`,
+  );
 
 //添加与修改已有品牌的方法
 export const reqAddOrUpdateTrademark = (data: TradeMark) => {
-    //修改已有品牌的数据
-    if (data.id) {
-        return request.put<any, any>(API.UPDATE_TRADEMARK_URL, data);
-    } else {
-        //新增品牌
-        return request.post<any, any>(API.ADD_TRADEMARK_URL, data);
-    }
-}
+  //修改已有品牌的数据
+  if (data.id) {
+    return request.put<any, any>(API.UPDATE_TRADEMARK_URL, data);
+  } else {
+    //新增品牌
+    return request.post<any, any>(API.ADD_TRADEMARK_URL, data);
+  }
+};
